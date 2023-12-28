68ae304457329066183facd8d776cd96ee2df23c
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-12-28 12:14:17 +0530

    Update MainActivity.java

--- app/src/main/java/com/nidhi/searchapplication/MainActivity.java
+++ app/src/main/java/com/nidhi/searchapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTextView;
 
     @Override 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
